7fb7ed1a1360870a45fe60b38fe0e58a9f9bef9d
androsv <satish.gitcode@gmail.com>
2020-10-08 23:34:39 +0530

    animation added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("nothing to see here......");
+const langSelector = document.querySelector("#lang-selector");
+const langArr = [
+  "Javascript",
+  "React",
+  "Node",
+  "Angular",
+  "Css",
+  "Html",
+  "Golang",
+  "Vue",
+];
+let num = 0;
+const changeText = () => {
+  if (num >= langArr.length) {
+    num = 0;
+  }
+  langSelector.textContent = langArr[num];
+  num++;
+};
+const change = setInterval(changeText, 1000);
